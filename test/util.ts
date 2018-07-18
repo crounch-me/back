0c5f160a32474e0a722d0a5d3ae05475e8d6be01
Sehsyha <valentin.stern@zenika.com>
2018-07-18 13:30:08 +0200

    Add session close on empty database

--- test/util.ts
+++ test/util.ts
@@ -4,6 +4,12 @@ export function emptyDatabase(): Promise<void> {
   const session = getSession()
   return session
     .run('MATCH (n) DETACH DELETE n')
-    .then(() => Promise.resolve())
-    .catch(err => Promise.reject(err))
+    .then(() => {
+      session.close()
+      Promise.resolve()
+    })
+    .catch(err => {
+      session.close()
+      Promise.reject(err)
+    })
 }
