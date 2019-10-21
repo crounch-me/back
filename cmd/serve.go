b4b50da25fb4639a0928f33ba89f0beba47907f6
Sehsyha <valentin.stern@gmail.com>
2019-10-21 11:57:15 +0200

    feat(cmd): add cmd arguments for DBConnectionURI and mock flag

---
+++ cmd/serve.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/Sehsyha/crounch-back/router"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var serveCmd = &cobra.Command{
+	Use:    "serve",
+	Short:  "Serve endpoints",
+	PreRun: initServeBindingFlags,
+	Run: func(cmd *cobra.Command, args []string) {
+		initConfig()
+
+		log.
+			WithField(parameterMock, config.Mock).
+			WithField(parameterDBConnectionURI, config.DBConnectionURI).
+			Warn("Configuration")
+
+		router.Start(config)
+	},
+}
+
+func init() {
+	serveCmd.Flags().Bool(parameterMock, defaultMock, "Use this flag to mock external services (such as db)")
+	serveCmd.Flags().String(parameterDBConnectionURI, defaultDBConnectionURI, "Use this flag to set the postgresql connection URI")
+}
+
+func initServeBindingFlags(cmd *cobra.Command, args []string) {
+	_ = viper.BindPFlag(parameterMock, cmd.Flags().Lookup(parameterMock))
+	_ = viper.BindPFlag(parameterDBConnectionURI, cmd.Flags().Lookup(parameterDBConnectionURI))
+}
---
+++ configuration/config.go
@@ -0,0 +1,7 @@
+package configuration
+
+type Config struct {
+	Mock bool
+
+	DBConnectionURI string
+}
