67ceb9bc099385865aa50273167691e85c61200e
Sehsyha <valentin.stern@gmail.com>
2020-02-07 16:10:47 +0100

    chore: small change to trigger CI

--- storage/mock/authorization.go
+++ storage/mock/authorization.go
@@ -2,6 +2,7 @@ package mock
 
 import "github.com/Sehsyha/crounch-back/model"
 
+// CreateAuthorization mocks create authorization
 func (sm *StorageMock) CreateAuthorization(u *model.User) (*model.Authorization, error) {
 	args := sm.Called(u)
 	return args.Get(0).(*model.Authorization), args.Error(1)
